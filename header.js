2993b73cdb0437eec8494c303e0608c99822c4be
blexc <alex.tchap99@gmail.com>
2022-07-18 16:55:51 -0700

    DRY method header

---
+++ header.js
@@ -0,0 +1,28 @@
+var html = `
+  <header>
+    <div class="block">
+      <h1 style="display: inline-block;">WELCOME TO MY EPIC SITE</h1>
+      <br>
+      <img src="./images/temmie.png" alt="temmie" style="width:30px; height: 27px; display: inline-block;"/>
+      <img src="./images/temmie.png" alt="temmie" style="width:30px; height: 27px; display: inline-block;"/>
+      <img src="./images/temmie.png" alt="temmie" style="width:30px; height: 27px; display: inline-block;"/>
+      <img src="./images/temmie.png" alt="temmie" style="width:30px; height: 27px; display: inline-block;"/>
+      <img src="./images/temmie.png" alt="temmie" style="width:30px; height: 27px; display: inline-block;"/>
+      <img src="./images/temmie.png" alt="temmie" style="width:30px; height: 27px; display: inline-block;"/>
+      <img src="./images/temmie.png" alt="temmie" style="width:30px; height: 27px; display: inline-block;"/>
+      <img src="./images/temmie.png" alt="temmie" style="width:30px; height: 27px; display: inline-block;"/>
+      <img src="./images/temmie.png" alt="temmie" style="width:30px; height: 27px; display: inline-block;"/>
+      <img src="./images/temmie.png" alt="temmie" style="width:30px; height: 27px; display: inline-block;"/>
+      <img src="./images/temmie.png" alt="temmie" style="width:30px; height: 27px; display: inline-block;"/>
+      <img src="./images/temmie.png" alt="temmie" style="width:30px; height: 27px; display: inline-block;"/>
+      <h3>Navigation</h3>
+      <ul>
+        <li><a href="index.html">Main Page</a></li>
+        <li><a href="aboutme.html">About Me</a></li>
+        <li><a href="favpictures.html">Pictures I like</a></li>
+      </ul>
+    </div>
+    <br>
+  </header>
+  `;
+document.write(html);
