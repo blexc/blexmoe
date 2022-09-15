91fe9133deffb3655e0391e98279813b55a21251
blexc <alex.tchap99@gmail.com>
2022-09-14 22:19:21 -0700

    no .html

--- header.js
+++ header.js
@@ -3,10 +3,10 @@ var html = `
     <div class="block">
       <h1 style="display: inline-block;">blex.moe</h1>
       <br>
-      | <a href="index.html">Main Page</a> | 
-      <a href="aboutme.html">About Me</a> | 
-      <a href="favpictures.html">Pictures</a> | 
-      <a href="ssroll.html">Super Sushi Roll</a> | 
+      | <a href="index">Main Page</a> | 
+      <a href="aboutme">About Me</a> | 
+      <a href="favpictures">Pictures</a> | 
+      <a href="ssroll">Super Sushi Roll</a> | 
       <a href="https://blexchapman.itch.io">Other Games</a> | 
     </div>
     <br>
