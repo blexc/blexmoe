441662db55b75d47e6563a26f5f457197cc14d99
blexc <alex.tchap99@gmail.com>
2022-09-14 22:20:36 -0700

    no index

--- header.js
+++ header.js
@@ -3,7 +3,7 @@ var html = `
     <div class="block">
       <h1 style="display: inline-block;">blex.moe</h1>
       <br>
-      | <a href="index">Main Page</a> | 
+      | <a href="https://blex.moe">Main Page</a> | 
       <a href="aboutme">About Me</a> | 
       <a href="favpictures">Pictures</a> | 
       <a href="ssroll">Super Sushi Roll</a> | 
