edb2b381d6438b647d7b1fd651220e43e519d89f
blexc <alex.tchap99@gmail.com>
2023-07-20 16:33:17 -0400

    change header to be centered

--- js/header.js
+++ js/header.js
@@ -1,11 +1,12 @@
 var html = `
   <header>
-    <div class="block">
-      <h1 style="display: inline-block;">blexChapman</h1>
-      <br>
-      | <a href="index.html">About Me</a> | 
-      <a href="ssroll.html">Super Sushi Roll</a> | 
-      <a href="https://blexchapman.itch.io">Other Games</a> | 
+    <div class="block" style="text-align: center; padding: 1px;">
+      <h1>blexChapman</h1>
+      <h4>
+        | <a href="index.html">About Me</a> | 
+        <a href="ssroll.html">Super Sushi Roll</a> | 
+        <a href="https://blexchapman.itch.io">Other Games</a> |
+      </h4>
     </div>
     <br>
   </header>
