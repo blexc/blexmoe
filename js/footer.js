bc79ab0c2edd5c54306766dbf8cbb687221f5e61
blexc <alex.tchap99@gmail.com>
2025-10-17 14:45:31 -0700

    2025

--- js/footer.js
+++ js/footer.js
@@ -1,7 +1,7 @@
 var html = `
   <footer>
     <div class="block">
-      <p>Website by <a href="https://twitter.com/blexchapman">blex</a> (c) 2023-24. Hosted by <a href="https://neocities.org">Neocities</a>.</p>
+      <p>Website by <a href="https://twitter.com/blexchapman">blex</a> (c) 2025. Hosted by <a href="https://neocities.org">Neocities</a>.</p>
     </div>
     <br>
   </footer>
--- js/header.js
+++ js/header.js
@@ -1,13 +1,11 @@
 var html = `
   <header>
     <div class="block" style="text-align: center; padding: 1px;">
-      <h1>Blex Chapman</h1>
-      <h4>
-        | <a href="index.html">About Me</a> | 
-        <a href="ssroll.html">Super Sushi Roll</a> | 
-        <a href="blog_index.html">Blog</a> | 
-        <a href="https://blexchapman.itch.io">Other Games</a> |
-      </h4>
+      <h1>blex's epic website</h1>
+      <h4> | <a href="index.html">About Me</a> 
+           | <a href="ssroll.html">Super Sushi Roll</a>
+           | <a href="blog_index.html">Blog</a> 
+           |</h4>
     </div>
     <br>
   </header>
